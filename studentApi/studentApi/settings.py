466b2c93b6d9de5cd4ab97e3de2ed6b0cd98577b
vinay kumar <vinay0001>
2020-07-17 16:11:18 +0530

    update

--- studentApi/studentApi/settings.py
+++ studentApi/studentApi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gky))zn&q&dxwzsx@kbpdm(l%#hzp3#jxw(#24-e-=g!13*2w+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
